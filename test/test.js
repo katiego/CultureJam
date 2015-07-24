1d10090fea843b8ebb529fdd63f7064c22414b59
katiego <katiegaudin@gmail.com>
2015-07-23 20:05:33 -0700

    added testing

--- test/test.js
+++ test/test.js
@@ -1,58 +1,55 @@
 var request = require('request'),
-    expect = require('chai').expect
+    expect = require('chai').expect,
+    baseUrl = 'http://localhost:3000';
 
 
  describe("Testing app functionality", function(){
 
 
-   it("Should successfully get users", function() {
-     request('http://localhost:3000/api/users', function(err, res, body) {
-       expect(users).to.exist;
+   it("Should successfully get users", function(done) {
+    request(baseUrl + '/api/users', function(err, res, body) {
+    expect(res.statusCode).to.equal(200);
      done();
        });
    });
 
-   it("Should successfully get songs", function(){
+
+   it("Should successfully get songs", function(done){
      request('http://localhost:3000/api/songs', function(err, res, body) {
-       expect(songs).to.exist;
-       done(); 
- 		});
+     expect(res.statusCode).to.equal(200);
+     done();
+       });
    });
 
-   it("Should get the target country's songs", function(){
-     request('http://localhost:3000/api/songs/Japan', function(err, res, json) {
-     app.get();
-     res.json.expect(songs.country).equal({country: "Japan"});
+   it("Should get the target country's songs", function(done){
+     request('http://localhost:3000/api/songs/India', function(err, res, json) {
+     json = JSON.parse(json) 
+     console.log('we are getting console from test', json[0].country);
+     // expect(res.statusCode).to.equal(200);
+     expect(json[0].country).to.equal("India");
      done(); 
      }); 
  	});
-
-   it("Should login the user", function(){
-     request('http://localhost:3000/login', function(err, res, body) {
-     var currentuser;
-     app.post();   
-     expect(currentuser).should.not.equal(null);
-     done (); 
-     }); 
+//stopped here
+   it("Should login the user", function(done){
+     request.post('http://localhost:3000/login', function(err, res, json) {   
+     expect(res.statusCode).to.equal(500);
+     done(); 
    });
+  });
 
    it("Should logout the user", function(){
      request('http://localhost:3000/logout', function(err, res, body) {
-     var currentuser;
-     app.get();   
-     expect(currentuser).should.equal(null);
-     console.log(currentuser);
-     done (); 
-     });
+     expect(res.statusCode).to.equal(500);
+     done(); 
    });
+  });
 
 	it("Should add the song to the array songs", function(){
      request('http://localhost:3000/api/songs', function(err, res, body) {
-     var songLength = songs.length	
-     app.post();   
-     expect(songs.length).equal(songLength + 1);
-     done (); 
-	 });
-    });
+     expect(res.statusCode).to.equal(500);
+     done(); 
+   });
+  });
 }); 
 
